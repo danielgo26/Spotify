3bd89c73e46d68521bc7ac283bc391b0081a5a57
Daniel Yordanov <62522030+danielgo26@users.noreply.github.com>
2025-04-09 10:07:31 +0300

    Update TextFormatHandler.java

--- src/data/format/TextFormatHandler.java
+++ src/data/format/TextFormatHandler.java
@@ -8,7 +8,7 @@ public interface TextFormatHandler {
 
     <T, K> String getFormat(Map<T, K> map);
 
-    <T, K> Map<T, K> loadMapFromFormat(String jsonString, Class<T> keyClass, Class<K> valueClass)
+    <T, K> Map<T, K> loadMapFromFormat(String formatString, Class<T> keyClass, Class<K> valueClass)
         throws InvalidTextFormatException;
 
 }
